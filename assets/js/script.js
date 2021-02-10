92a7cd822880c07d01a5e3921064f2c2ddc4a6b5
Pavel Gryanko <pavelgruanko23@gmail.com>
2021-02-10 16:05:23 +0300

    fixed-index

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,7 +15,25 @@ $(document).ready(function () {
   $('ul.blocks-gallery-grid li a').attr("data-fancybox","gallery");
   $('.wp-block-media-text__media a').attr("data-fancybox","gallery");
  */
-
+    var show = true;
+    var countbox = ".show-count";
+    $(window).on("scroll load resize", function () {
+        if (!show) return false; // Отменяем показ анимации, если она уже была выполнена
+        var w_top = $(window).scrollTop(); // Количество пикселей на которое была прокручена страница
+        var e_top = $(countbox).offset().top; // Расстояние от блока со счетчиками до верха всего документа
+        var w_height = $(window).height(); // Высота окна браузера
+        var d_height = $(document).height(); // Высота всего документа
+        var e_height = $(countbox).outerHeight(); // Полная высота блока со счетчиками
+        if (w_top + 500 >= e_top || w_height + w_top == d_height || e_height + e_top < w_height) {
+            $('.sec-about-numbers__count').css('opacity', '1');
+            $('.sec-about-numbers__count').spincrement({
+                thousandSeparator: "",
+                duration: 1200
+            });
+            show = false;
+        }
+    });
+ 
   // Кнопка мобильного меню header
   $("a.header__menu-btn-mob").on('click', function (e) {
     e.preventDefault();
